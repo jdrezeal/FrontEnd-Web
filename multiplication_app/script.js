68aecdb7ef6ec8c8d18c5d62ed13d6b6172a214d
jdrezeal <148607949+jdrezeal@users.noreply.github.com>
2024-01-11 04:50:00 +0000

    add btn id, disable and enable submit button, reset score value

--- multiplication_app/script.js
+++ multiplication_app/script.js
@@ -4,17 +4,32 @@ let score = JSON.parse(localStorage.getItem("score"));
 if(!score){
     score = 0;
 }
+let cnt = JSON.parse(localStorage.getItem("cnt"));
+if(!cnt){
+    cnt = 0;
+}
 const questionEl = document.getElementById("question");
 const formEl = document.getElementById("form");
 const inputEl = document.getElementById("input");
 const scoreEl = document.getElementById("score");
+const submitBtn = document.getElementById("btn");
 questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
 scoreEl.innerText = `Score: ${score}`;
 
 const ans = num1 * num2;
+if (isNaN(inputEl.value)){
+    submitBtn.disabled=true;
+}else {
+    submitBtn.disabled=false;
+}
 
 formEl.addEventListener("submit",()=>{
     const userInput = +inputEl.value;
+
+    if (isNaN(userInput)){
+        alert('Please enter valid value!');
+    }
+
     if (userInput === ans){
         score++;
         update();
@@ -22,8 +37,17 @@ formEl.addEventListener("submit",()=>{
         score--;
         update();
     }
+    cnt++;
+    update()
+    if (cnt === 5){
+        alert('Final Score:'+score);
+        score = 0;
+        cnt = 0;
+        update();
+    }
 });
 
 function update(){
     localStorage.setItem("score",JSON.stringify(score));
+    localStorage.setItem("cnt",JSON.stringify(cnt));
 }
