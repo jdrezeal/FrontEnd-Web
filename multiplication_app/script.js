bd6329e169f166a572463440ec46285bfd6c4125
jdrezeal <148607949+jdrezeal@users.noreply.github.com>
2024-01-11 06:24:44 +0000

    update

--- multiplication_app/script.js
+++ multiplication_app/script.js
@@ -1,13 +1,12 @@
 const num1 = Math.ceil(Math.random()*10);
 const num2 = Math.ceil(Math.random()*10);
 let score = JSON.parse(localStorage.getItem("score"));
+let cnt = JSON.parse(localStorage.getItem("cnt"));
 if(!score){
     score = 0;
-}
-let cnt = JSON.parse(localStorage.getItem("cnt"));
-if(!cnt){
     cnt = 0;
 }
+
 const questionEl = document.getElementById("question");
 const formEl = document.getElementById("form");
 const inputEl = document.getElementById("input");
@@ -15,36 +14,47 @@ const scoreEl = document.getElementById("score");
 const submitBtn = document.getElementById("btn");
 questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
 scoreEl.innerText = `Score: ${score}`;
-
+console.log('input is ' + inputEl.value);
 const ans = num1 * num2;
-if (isNaN(inputEl.value)){
-    submitBtn.disabled=true;
-}else {
-    submitBtn.disabled=false;
-}
 
 formEl.addEventListener("submit",()=>{
     const userInput = +inputEl.value;
 
-    if (isNaN(userInput)){
+    if (isNaN(userInput)|| inputEl.value === ''){
         alert('Please enter valid value!');
-    }
-
-    if (userInput === ans){
-        score++;
-        update();
-    }else{
-        score--;
-        update();
-    }
-    cnt++;
-    update()
-    if (cnt === 5){
-        alert('Final Score:'+score);
+        localStorage.clear();
         score = 0;
         cnt = 0;
         update();
+    }else{
+        if (userInput === ans){
+            score++;
+            update();
+        }else{
+            score--;
+            update();
+        }
+        
+        if(score < 0){
+            alert('Game Over !!!');
+            localStorage.clear();
+            score = 0;
+            cnt = 0;
+            update();
+        }
+        cnt++;
+        update()
+        if (cnt === 5){
+            alert('Final Score:'+score);
+            localStorage.clear();
+            score = 0;
+            cnt = 0;
+            update();
+        }
     }
+
+    
+
 });
 
 function update(){
